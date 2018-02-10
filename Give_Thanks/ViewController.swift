05e29ce35d749c6b6b96afc1e3418aa2b0b329ef
Sandeep Joshi <aw.snjoshi@hotmail.com>
2018-02-10 17:09:01 -0600

    Initial Commit

---
+++ Give_Thanks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Give_Thanks
+//
+//  Created by Sandeep Joshi on 2/10/18.
+//  Copyright © 2018 Sandeep Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
