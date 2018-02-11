bef5caed5bd24e7ce137f52ee168fa62ac63df78
Sandeep Joshi <aw.snjoshi@hotmail.com>
2018-02-11 00:50:37 -0600

    changes....

---
+++ Give_Thanks/LeaderBoardTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeaderBoardTableCell.swift
+//  Give_Thanks
+//
+//  Created by Sandeep Joshi on 2/10/18.
+//  Copyright © 2018 Sandeep Joshi. All rights reserved.
+//
+
+import UIKit
+
+class LeaderBoardTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
