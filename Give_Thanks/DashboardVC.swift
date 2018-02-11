b285371ed96f845887bdbb70403947dd948808df
Sandeep Joshi <aw.snjoshi@hotmail.com>
2018-02-10 21:03:21 -0600

    Changes in Dashboard

--- Give_Thanks/DashboardVC.swift
+++ Give_Thanks/DashboardVC.swift
@@ -10,39 +10,55 @@ import UIKit
 import Charts
 
 class DashboardVC: ViewController {
-
+    
+    @IBOutlet weak var user: UILabel!
+    @IBOutlet weak var memberSince: UILabel!
+    @IBOutlet weak var donations: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var barView: BarChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-        var dataEntries: [BarChartDataEntry] = []
+        profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width / 2;
+        profileImageView.layer.borderColor = UIColor.black.cgColor
+        profileImageView.layer.borderWidth = 1.5
+        profileImageView.clipsToBounds = true
+        profileImageView.image = UIImage(named: "bill")
         
-        for i in 0..<35 {
-            let dataEntry = BarChartDataEntry(x: Double(i), y: Double(arc4random_uniform(42)))
-            dataEntries.append(dataEntry)
-        }
-        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Visitor count")
-        let chartData = BarChartData(dataSet: chartDataSet)
-        barView.data = chartData
+        user.text = "bill@MSFT"
+        memberSince.text = "2007"
+        donations.text = "$15,000"
+        name.text = "Bill Gates"
         
+        
+        //        var dataEntries: [BarChartDataEntry] = []
+        //
+        //        for i in 0..<35 {
+        //            let dataEntry = BarChartDataEntry(x: Double(i), y: Double(arc4random_uniform(42)))
+        //            dataEntries.append(dataEntry)
+        //        }
+        //        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Visitor count")
+        //        let chartData = BarChartData(dataSet: chartDataSet)
+        //        barView.data = chartData
+        //
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
